6a7efa6f5a17f8d5181ff2d11b8d7398980fddf9
Andres <andreslabat89@gmail.com>
2023-06-08 13:08:43 +0200

    ultimo commit

---
+++ src/ControlDeVersiones/paraborrar5.java
@@ -0,0 +1,13 @@
+package ControlDeVersiones;
+
+public class paraborrar5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("matame por favor x2");
+
+		
+	}
+
+}
