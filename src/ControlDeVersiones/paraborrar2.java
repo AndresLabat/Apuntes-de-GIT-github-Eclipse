b547257fec57d5bc27ba6ef4c675dca17066c1e8
Andres <andreslabat89@gmail.com>
2023-06-08 12:54:58 +0200

    añadidos 3 archivos paras borrar

---
+++ src/ControlDeVersiones/paraborrar2.java
@@ -0,0 +1,12 @@
+package ControlDeVersiones;
+
+public class paraborrar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("tambien para borrar");
+		
+	}
+
+}
